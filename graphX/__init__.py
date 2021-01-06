dc8a71cf3fd825578aa71a34565037e05332623f
Darcy LF <ehnevault@gmail.com>
2021-01-06 13:27:51 +1100

    removed useless comment about algx

--- graphX/__init__.py
+++ graphX/__init__.py
@@ -145,27 +145,3 @@ if __name__ == "__main__":
         g.del_edge("b", "a")
         #g._dispactch_dict({})
     start()
-
-
-""" # Create a new HTTP server on port 5050
-server = ax.http_server(port=4000)
-# Create a new canvas
-canvas = server.canvas()
-
-def start():
-    # Use the library normally, for example:
-    canvas.nodes([1, 2]).add()
-    canvas.edge((1, 2)).add()
-
-    canvas.pause(1)
-
-    canvas.node(1).highlight().size('1.5x').pause(0.5)
-    canvas.edge((1, 2)).traverse('blue')
-
-# A 'start' message is sent by the client whenever the
-# user clicks the start or restart button
-canvas.onmessage('start', start)
-
-# Start the server, blocking all further execution on
-# the current thread. Use 'ctrl-c' to exit the script.
-server.start() """
