05ff58619a6ada66f18c662a70416953615ee33c
Darcy LF <ehnevault@gmail.com>
2021-01-29 21:47:31 +1100

    fix style problems

--- graphX/__init__.py
+++ graphX/__init__.py
@@ -2,8 +2,10 @@ import algorithmx as ax
 from pathlib import Path
 import os
 
+
 def _remove_dupes(x):
-  return list(dict.fromkeys(x))
+    return list(dict.fromkeys(x))
+
 
 class Node:
     def __init__(self, val):
@@ -16,7 +18,7 @@ class Node:
 
     def __str__(self):
         """This is the string representation of the node, and will be used in setting edges and getting specific nodes"""
-        return self.val  
+        return self.val
 
     def set_attr(self, attr, val):
         """sets a nodes specific attribute (eg. seen, size, etc.)"""
@@ -25,25 +27,36 @@ class Node:
     def get_attr(self, attr):
         """returns an attribute"""
         return self.attrs[attr]
-    
+
     def get_size(self):
         """returns the approximate length the node's graph representation would have to be to show all the text"""
-        size = 0 # in milinches
+        size = 0  # in milinches
         for s in self.val:
-            if s in 'lij|\' ': size += 1.3
-            elif s in '![]fI.,:;/\\t': size += 2
-            elif s in '`-(){}r"': size += 2
-            elif s in '*^zcsJkvxy': size += 3
-            elif s in 'aebdhnopqug#$L+<>=?_~FZT0123456789': size += 3.5
-            elif s in 'BSPEAKVXY&UwNRCHD': size += 4
-            elif s in 'QGOMm%W@—': size += 5
-            else: size += 5
+            if s in "lij|' ":
+                size += 1.3
+            elif s in "![]fI.,:;/\\t":
+                size += 2
+            elif s in '`-(){}r"':
+                size += 2
+            elif s in "*^zcsJkvxy":
+                size += 3
+            elif s in "aebdhnopqug#$L+<>=?_~FZT0123456789":
+                size += 3.5
+            elif s in "BSPEAKVXY&UwNRCHD":
+                size += 4
+            elif s in "QGOMm%W@—":
+                size += 5
+            else:
+                size += 5
         # above code slightly adjusted from: https://stackoverflow.com/questions/16007743/roughly-approximate-the-width-of-a-string-of-text-in-python
         # print("node" ,f"'{self}'", "length before padding", size, "after", size + 5)
         return size + 5
 
+
 class Graph:
-    def __init__(self, is_jupyter=False, port=5050, width=300, height=200, custom_ui=None):
+    def __init__(
+        self, is_jupyter=False, port=5050, width=300, height=200, custom_ui=None
+    ):
         self.nodes = {}
         self.edges = {}
         self.canvas = ""
@@ -57,38 +70,43 @@ class Graph:
             if custom_ui is None:
                 # finds the interface html. This is more complicated than it should be
                 base_path = os.path.relpath(__file__)
-                #custom_ui = str((base_path / "/ui.html").resolve())
+                # custom_ui = str((base_path / "/ui.html").resolve())
                 custom_ui = f"{Path(base_path).parent}/ui.html"
             print("serving ui found at", custom_ui)
             self.server = ax.http_server(port=port, file=custom_ui)
             self.canvas = self.server.canvas()
-            
 
     # boring running stuff for web
     def run_web(self, func):
         def wrapper():
-            self.canvas.onmessage('start', func)
-            print(f"staring server on http://localhost:{self.port} — press ctrl+c to quit")
+            self.canvas.onmessage("start", func)
+            print(
+                f"staring server on http://localhost:{self.port} — press ctrl+c to quit"
+            )
             self.server.start()
+
         return wrapper
 
     def _dispactch_dict(self, dispatch_dict):
-        self.canvas.dispatch({"hello":"cool"})
+        self.canvas.dispatch({"hello": "cool"})
+
     # Nodes
     def add_node(self, n):
         """adds node "n" to the graph"""
         self.nodes[n] = Node(n)
-        self.canvas.node(Node(n)).add(color="#423f39", size=(Node(n).get_size(),12), shape="rect")
+        self.canvas.node(Node(n)).add(
+            color="#423f39", size=(Node(n).get_size(), 12), shape="rect"
+        )
 
     def add_nodes(self, n_list):
         """runs "add_node" for each node in the iterable"""
         for n in n_list:
             self.add_node(n)
-    
+
     def get_nodes(self):
         """returns a list of all the nodes in a graph"""
         return [self.nodes[i] for i in self.nodes]
-    
+
     def get_node(self, val):
         """returns a specific node"""
         return self.nodes[val]
@@ -97,6 +115,7 @@ class Graph:
         """deletes a specified node"""
         self.nodes.pop(val, None)
         self.canvas.node(val).remove()
+
     # Edges
     def add_edge(self, source, target, value=1, directed=False):
         """adds edges between source and target nodes. if it isn't directed it will also add a target to source edge."""
@@ -128,10 +147,11 @@ class Graph:
         """plays a little animation going from the source node to the target node along their edge"""
         self.canvas.edge((source, target)).traverse(color)
 
-    # general, graph-wide, methods 
+    # general, graph-wide, methods
     def pause(self, time=1):
         """pauses the animation for `time` seconds"""
         self.canvas.pause(time)
+
 
 if __name__ == "__main__":
     g = Graph()
@@ -160,14 +180,15 @@ if __name__ == "__main__":
             g.add_edge(n1, n)
             g.add_edge(n2, n) """
         g.add_node("—")
-        
-        g.add_node('lij|\' ')
-        g.add_node('![]fI.,:;/\\t')
+
+        g.add_node("lij|' ")
+        g.add_node("![]fI.,:;/\\t")
         g.add_node('`-(){}r"')
-        g.add_node('*^zcsJkvxy')
-        g.add_node('aebdhnopqug#$L+<>=?_~FZT0123456789')
-        g.add_node('BSPEAKVXY&UwNRCHD')
-        g.add_node('QGOMm%W@—')
-        
-        #g._dispactch_dict({})
+        g.add_node("*^zcsJkvxy")
+        g.add_node("aebdhnopqug#$L+<>=?_~FZT0123456789")
+        g.add_node("BSPEAKVXY&UwNRCHD")
+        g.add_node("QGOMm%W@—")
+
+        # g._dispactch_dict({})
+
     start()
